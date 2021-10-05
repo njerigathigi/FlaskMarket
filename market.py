c740f16370141af94a0373b0d9e6378616b4c5f8
njerigathigi <njeriiterry@gmail.com>
2021-10-05 20:41:57 +0300

    replacing dictionary with items from the database

--- market.py
+++ market.py
@@ -6,11 +6,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 db = SQLAlchemy(app)
 
 class Item(db.Model):
-    id = db.Column(db.integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Interger(), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description= db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"{self.name}"
+
 
 @app.route("/")
 @app.route("/home")
@@ -19,9 +23,5 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-]
+    items = Item.query.all()
     return render_template("market.html", items=items)
