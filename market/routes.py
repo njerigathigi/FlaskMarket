c2b32f22e1fc0163a14f03013f9ca3311ead5c75
njerigathigi <njeriiterry@gmail.com>
2021-10-19 20:09:44 +0300

    adding register route

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 
 @app.route("/")
 @app.route("/home")
@@ -11,4 +12,9 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
-    
+
+@app.route("/register")
+def register_page():
+    #generate secret key using import os, os.urandom(12).hex() in python shell.
+    form = RegisterForm()
+    return render_template('register.html', form=form)
