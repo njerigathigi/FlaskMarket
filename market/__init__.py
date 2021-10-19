acd530530af50601c45e430596eb7e9f9d124231
njerigathigi <njeriiterry@gmail.com>
2021-10-19 20:09:15 +0300

    adding secret key

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db" #name of database file #standard #tells app where to find db
+app.config["SECRET_KEY"] = 'd65c5cbc4d268a8a431a06d5' #The secret key is needed to keep the client-side sessions secure. 
+
 db = SQLAlchemy(app)
 
 from market import routes
@@ -10,3 +12,4 @@ from market import routes
 #A directory must contain a file named __init__.py in order for Python 
 #to consider it as a package. This file can be left empty but we generally 
 #place the initialization code for that package in this file.
+ 
