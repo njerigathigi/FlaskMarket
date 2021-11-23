919a42d779b8d2c652cf3e046a6d3bd68f7233d4
njerigathigi <njeriiterry@gmail.com>
2021-11-23 12:49:06 +0300

    change password_hash to password

--- market/routes.py
+++ market/routes.py
@@ -25,7 +25,7 @@ def register_page():
     if form.validate_on_submit(): #checks if user has clicked on the submit button of the form. #info is validated
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit() 
         return redirect(url_for("market_page"))#expects a hardcoded url but url_for helps us navigate this.
@@ -39,8 +39,8 @@ def register_page():
 def login_page():
     form = LoginForm()
     if form.validate_on_submit():
-        attempted_user = User.query.get(form.username.data).first()
-        if attempted_user and attempted_user.check_password_correctness(form.password.data):
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correctness(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f"Success! You are logged in as: {attempted_user.username}", category="success")
             return redirect(url_for("market_page"))
