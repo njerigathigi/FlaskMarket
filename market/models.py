0508c4d0e3a34ddb9a1426a8b7c802df5773e0f2
njerigathigi <njeriiterry@gmail.com>
2021-11-23 17:01:16 +0300

    check if budget is 4 figures. if True add coma between the digits

--- market/models.py
+++ market/models.py
@@ -13,7 +13,14 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget  = db.Column(db.Integer(),nullable=False, default=1000)
     items = db.relationship('Item', backref = "owned_user", lazy=True)
-
+    
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f"{str(self.budget)[:-3]},{str(self.budget)[-3:]}$"
+        else:
+            return f"{self.budget}$"
+    
     @property
     def password(self):
         return self.password
