174601e43a718252ad346055504043159d0be1a5
njerigathigi <njeriiterry@gmail.com>
2021-11-25 20:02:47 +0300

    create purchase_form instance in market page round and render it to market.html

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required
  
@@ -13,14 +13,17 @@ def home_page():
 @app.route("/market")
 @login_required
 def market_page():
+
+    purchase_form = PurchaseItemForm()
     items = Item.query.all()
-    return render_template("market.html", items=items)
+    return render_template("market.html", items=items, purchase_form=purchase_form)
 
  #allow route to handle web requests.(post and get)
  #get- send and the server returns data.
  #post- used to send html form data to the server.
 @app.route("/register", methods=["GET", "POST"])
 def register_page():
+    
     #generate secret key using import os, os.urandom(12).hex() in python shell.
     form = RegisterForm()
     if form.validate_on_submit(): #checks if user has clicked on the submit button of the form. #info is validated
@@ -32,6 +35,7 @@ def register_page():
         login_user(user_to_create)
         flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
         return redirect(url_for("market_page"))#expects a hardcoded url but url_for helps us navigate this.
+    
     if form.errors != {}: #this is a dictionary
         for error_message in form.errors.values(): #can access values through the values() dict class method.
             flash(f"There was an error with creating a user:{error_message}", category="danger") #error_message is a list.
@@ -40,9 +44,13 @@ def register_page():
 
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
+    
     form = LoginForm()
+    
     if form.validate_on_submit():
+        
         attempted_user = User.query.filter_by(username=form.username.data).first()
+        
         if attempted_user and attempted_user.check_password_correctness(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f"Success! You are logged in as: {attempted_user.username}", category="success")
@@ -54,6 +62,7 @@ def login_page():
 
 @app.route('/logout')
 def logout_page():
+    
     logout_user()
     flash("You have been logged out", category="info")
     return redirect(url_for("home_page"))
